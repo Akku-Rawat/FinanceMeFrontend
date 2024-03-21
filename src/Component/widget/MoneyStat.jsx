04aea437913a6bf44d2d213a10fdfaec230eb03c
Akhilesh Rawat <akhileshrawat.asr@gmail.com>
2024-03-21 11:26:33 +0530

    Update MoneyStat.jsx
    color change

--- src/Component/widget/MoneyStat.jsx
+++ src/Component/widget/MoneyStat.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function MoneyStat(prop) {
   return (
-    <div className="flex w-full h-12 lg:h-24  mb-2 lg:w-96 md:h-16 bg-gradient-to-r from-indigo-300 via-purple-300 to-pink-300  rounded-xl transform transition-all hover:-translate-y-1 duration-300 shadow-lg hover:shadow-xl z-1">
+    <div className="flex w-full h-12 lg:h-24  mb-2 lg:w-96 md:h-16 bg-gradient-to-r from-indigo-200 via-purple-200 to-pink-320  rounded-xl transform transition-all hover:-translate-y-1 duration-300 shadow-lg hover:shadow-xl z-1">
       <h1 className="justify-items-center items-center m-auto text-lg md:text-2xl font-sans">
         {prop.title}
       </h1>
